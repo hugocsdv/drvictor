b150f286b8f0a07147ac29b8d16204b953dde006
Hugo Sanches <74740073+hugocsdv@users.noreply.github.com>
2026-09-18 02:19:57 -0300

    foi

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,26 +2,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-  const token =
-    request.cookies.get("access_token")?.value;
+  const token = request.cookies.get("access_token")?.value;
 
   console.log("🔥 PROXY:", request.nextUrl.pathname);
-  console.log("🍪 COOKIES:", request.cookies.getAll());
-  console.log("🔑 TOKEN:", !!token);
+  console.log("TOKEN:", !!token);
 
   if (!token) {
-    console.log("❌ SEM TOKEN -> LOGIN");
-
     return NextResponse.redirect(
-      new URL("/login", request.url),
+      new URL("/login", request.url)
     );
   }
-
-  console.log("✅ TOKEN ENCONTRADO -> DASHBOARD");
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: "/dashboard",
 };
